8df40d7a582f11c5b9120d6d208442caad191595
Mahdiald <Mahdiald.2003@gmail.com>
2026-02-15 18:07:11 +0100

    15/02

--- src/main.java
+++ src/main.java
@@ -1,2 +1,11 @@
-public class main {
+void main(){
+    String name = "Kratos";
+    int currentHealth = 85;
+    int maxHealth = 100;
+    int level = 5;
+    int xp = 2300;
+    double gold = 156.50;
+    boolean alive = true;
+    String[] type = {"W", "M", "R"};
+
 }
